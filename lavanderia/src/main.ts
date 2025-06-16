13fdeb0cc02b465bcb698be9a2a92f405810844c
OscarTum <oscar.tumbaco01@epn.edu.ec>
2025-06-16 16:10:52 -0500

    Nest Lavanderia

---
+++ lavanderia/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap()
+  .then(() => console.warn('Servidor levantado correctamente'))
+  .catch(() => console.error('Error al levantar el servidor'));
